3e6267febf1281e6c64b2088deae3b69dddc4f17
Olga Shevchenko <duvasu7777@gmail.com>
2022-01-06 00:48:34 +0200

    Task №1
    Task №1

---
+++ js/01-gallery.js
@@ -0,0 +1,58 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick)
+
+function createGalleryMarkup(galleryItems) {
+   
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    }).join(" ");
+}
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isGalleryImage = evt.target.classList.contains("gallery__image")
+    if (!isGalleryImage) {
+        return;
+    }
+    const selectedImage = evt.target.dataset.source;
+    console.log(selectedImage);
+
+    const modal = basicLightbox.create(`
+    <div class="modal">
+    <img
+    class="gallery__image"
+     src="${selectedImage}"
+     />
+     </div>
+    `);  
+    modal.show()
+    window.addEventListener("keydown", onEscPress);
+    
+    function onEscPress(e) {
+        if (e.key === "Escape") {
+            modal.close()
+        }
+    }
+}
