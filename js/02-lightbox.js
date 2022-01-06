476f9a51ec1dd2c2619712744bf688bb0573ff5c
Olga Shevchenko <duvasu7777@gmail.com>
2022-01-06 22:02:23 +0200

    Task #2
    Task #2 for hw 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,36 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+function createGalleryMarkup(galleryItems) {
+   
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>
+`;
+    }).join(" ");
+}
+
+const captionOptions = {
+    captions: true,
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+}
+let gallery = new SimpleLightbox('.gallery a', captionOptions);
+gallery.on('show.simplelightbox', function () {
+});
